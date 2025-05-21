f4543125a4cb871b6624d97e6a5859b1027346d2
Sh1ng0 <reye31416@gmail.com>
2025-05-21 19:07:42 +0200

    Initial commit: Setup monorepo for microservices

---
+++ bets/bets/src/test/java/com/bjpractice/bets/BetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjpractice.bets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
